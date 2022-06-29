53088ffdcf8cbdcf108dd61d8ace58f44938f213
Justin Baker <100954109+justinBak@users.noreply.github.com>
2022-06-29 12:30:57 -0400

    Make modifications to gitignore file

--- src/Components/More.js
+++ src/Components/More.js
@@ -8,9 +8,11 @@ function More() {
     <div className="app">
       <div className='content-container row'>
         <div className='more-content-ct'>
-          <img width="80px" src='https://play-lh.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM=w600-h300-pc0xffffff-pd'></img>
+          <img width="92px" src='https://play-lh.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM=w600-h300-pc0xffffff-pd'></img>
           <p style={{marginLeft: '18px', fontWeight: 'bold'}}>Spotify</p>
-          <button className='btn-style'><a className='spotify-btn' href='https://open.spotify.com/' target="_blank">Spotify</a></button>
+          <button className='btn-style'>
+            <a className='spotify-btn' href='https://open.spotify.com/' target="_blank">Spotify</a>
+          </button>
         </div>
       </div>
     </div>
